91a0deb54578ed0b0b6ab205d1254bff174b98d1
Rahul Rawat <rahul24920@gmail.com>
2024-11-11 13:41:39 +0530

    clerk setup finish

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div><UserButton /></div>
   );
 }
--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from '@clerk/nextjs'
+import Image from 'next/image'
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div>
+      <Image src='/uber_banner.png' alt='uber-banner' width={900} height={900} className='object-contain h-full w-full' />
+      <div className='absolute top-20 right-10'>
+        <SignIn />
+      </div>
+    </div>
+  )
 }
