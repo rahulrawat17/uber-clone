0a2125e1c14c511b3474c461cac0fc7df3ef8af1
Rahul Rawat <rahul24920@gmail.com>
2025-02-16 17:15:20 +0530

    Add Google Maps integration with search functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import { UserButton } from "@clerk/nextjs";
+import GoogleMapSection from "@/components/Home/GoogleMapSection";
+import SearchSection from "@/components/Home/SearchSection";
 
 export default function Home() {
   return (
-    <div><UserButton /></div>
+    <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
+      <div>
+        <SearchSection />
+      </div>
+      <div className="col-span-2">
+        <GoogleMapSection />
+      </div>
+    </div>
   );
 }
