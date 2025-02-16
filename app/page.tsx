94f901b57179676db7f15fd5610002d9575818cd
Rahul Rawat <rahul24920@gmail.com>
2025-02-16 17:18:46 +0530

    test commit 3

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import GoogleMapSection from "@/components/Home/GoogleMapSection";
 import SearchSection from "@/components/Home/SearchSection";
 
 export default function Home() {
+  console.log("🚀 ~ file: Home.tsx:15 ~ Home ~ test3:")
   return (
     <div className="p-6 grid grid-cols-1 md:grid-cols-3 gap-5">
       <div>
